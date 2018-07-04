9b102f603126aa264ae527b4d0f1cd5f1cfe32e2
Anna van Bohemen <anna@vanbohemen.com>
2018-07-04 14:36:53 +0200

    active player style switch

--- app.js
+++ app.js
@@ -42,5 +42,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore = 0
         document.querySelector('#current-' + activePlayer ).textContent = roundScore;
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
     }
 })
